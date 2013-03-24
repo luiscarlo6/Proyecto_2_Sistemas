d1cbdb833511aef2c5cc79946519a2827a150acf
josejulian <eljjpr416@hotmail.com>
2013-03-23 19:39:11 -0430

    Ya leo todos los directorios

--- main.c
+++ main.c
@@ -20,25 +20,84 @@
 
 #define MAX 1000
 
+char *NombreDirectorio = NULL;
+char NombreDirectorioActual[MAX];
+
+int Es_Directorio(char* Nombre);
+
+void Procesar_Directorio(char* Nombre);
+
+int NumHijos=0;
+
+
+
+
 int main(int argc, char **argv) {
   
-  DIR *dirp;
-  struct dirent *direntp;
-  struct stat statbuf;
-  
-  char *NombreDirectorio = NULL;
-  char NombreDirectorioActual[MAX];
-  
-  int Tam = -1; /*variable que contendra el size de argv[1]*/
-  int i = 0; /*Contador multipropositos*/
-  int Tam1 = -1; /*variable que contendra el size de NombreDirectorio*/
+  static int i=0;
   
   if (argc < 2 || argc > 2) {
 	 printf("\nError en la linea de argumentos\n\n");
 	 exit(1);
   }
   
-  Tam = strlen(argv[1]);
+  if(!Es_Directorio(argv[1])){
+	 printf("El argumento %s\tNO ES UN DIRECTORIO",argv[1]);
+	 exit(1);
+  }
+  
+  Procesar_Directorio(argv[1]);
+  
+  for(i=0; i!=NumHijos; i++){
+	 wait();
+  }
+  
+  printf("SOY EL MAIN \t%s\n",NombreDirectorio);
+  
+  free(NombreDirectorio);
+  
+  return 0;
+}
+
+
+
+int Es_Directorio(char* Nombre){
+  
+  struct stat statbuf;
+  /**/
+  if (lstat(Nombre, &statbuf) == 1) {
+	 fprintf(stderr, " No se pudo aplicar stat sobre el archivo %s: %s \n", Nombre, strerror(errno));
+	 exit(1);
+  }
+  
+  /*verifico si es directorio*/
+  if ( statbuf.st_mode & S_IFDIR){
+	 printf("%s\t es un directorio\n",Nombre);
+	 return 1;
+  }
+  else{
+	 printf("%s\t no es un directorio\n",Nombre);
+	 return 0;
+  }
+  
+  return -1;
+  
+}
+
+
+void Procesar_Directorio(char* Nombre){
+  
+  DIR *dirp;
+  struct dirent *direntp;
+  
+  //char *NombreDirectorio = NULL;
+  
+  
+  int Tam = -1; /*variable que contendra el size de argv[1]*/
+  int i = 0; /*Contador multipropositos*/
+  int Tam1 = -1; /*variable que contendra el size de NombreDirectorio*/
+  
+  Tam = strlen(Nombre);
   
   Tam1 = Tam + 1;
   
@@ -56,12 +115,14 @@ int main(int argc, char **argv) {
   }
   
   /*Copiamos los n elementos de argv[1] en NombreDirectorio*/
-  strncpy(NombreDirectorio, argv[1], Tam);
+  strncpy(NombreDirectorio, Nombre, Tam);
+  
+  printf("HOLA SOY %s\t%d\n",NombreDirectorio,getppid());
   
   
   /*se abre el directorio*/
-  if ((dirp = opendir(argv[1])) == NULL) {
-	 fprintf(stderr, "No se pudo abrir el directorio %s: %s", argv[1], strerror(errno));
+  if ((dirp = opendir(NombreDirectorio)) == NULL) {
+	 fprintf(stderr, "No se pudo abrir el directorio %s: %s", NombreDirectorio, strerror(errno));
 	 exit(1);
   }
   
@@ -72,9 +133,9 @@ int main(int argc, char **argv) {
 	 if(strcmp (direntp->d_name, ".") != 0 &&  strcmp (direntp->d_name, "..") != 0){
 		
 		/*inicializo la variable en un string vacio*/
-		 for (i = 0; i != MAX; i++) {
-			 NombreDirectorioActual[i] = '\0';
-		 }
+		for (i = 0; i != MAX; i++) {
+		  NombreDirectorioActual[i] = '\0';
+		}
 		
 		/*coloco el directorio padre*/
 		strcpy(NombreDirectorioActual, NombreDirectorio);
@@ -82,24 +143,29 @@ int main(int argc, char **argv) {
 		strcat(NombreDirectorioActual, "/");
 		strcat(NombreDirectorioActual, direntp->d_name);
 		
-		/**/
-		if (lstat(NombreDirectorioActual, &statbuf) == 1) {
-		  fprintf(stderr, " No se pudo aplicar stat sobre el archivo %s: %s \n", direntp->d_name, strerror(errno));
-		  exit(1);
-		}
-		
-		/*verifico si es directorio*/
-		if ( statbuf.st_mode & S_IFDIR){
-		  printf("%s es un directorio\n",direntp->d_name);
-		}
-		else{
-		  printf("%s no es un directorio\n",direntp->d_name);
+		if(Es_Directorio(NombreDirectorioActual)){
+		  
+		  if(fork() == 0 ){
+			 
+			 Procesar_Directorio(NombreDirectorioActual);
+			 
+			 for(i=0;i!=NumHijos;i++){
+				wait();
+			 }
+			 
+			 printf("SOY \t%s\n",NombreDirectorio);
+			 free(NombreDirectorio);
+			 exit(0);
+			 
+		  }else{
+			 
+			 NumHijos++;
+			 
+		  }
 		}
 		
 	 }/*fin de if*/
   }/*fin del for*/
   
   closedir(dirp);
-
-  return 0;
 }
